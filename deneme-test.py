48df266dc637d5f423a3c86d75ca4955cb196304
emrekose <koseeemre2@gmail.com>
2023-06-14 23:15:45 +0300

    stash-deneme

--- deneme-test.py
+++ deneme-test.py
@@ -3,6 +3,7 @@ from deneme import Add
 def TestAdd():
     assert Add(2,3) == 5
     assert Add(27,3) == 6
+    assert Add(27,3) == 7
     print("add function works correctly")
     
 if __name__ == '__main__':
